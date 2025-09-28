61325c8d73ef2a6e8ed98364127c0b1ca62ac4c4
Chance Jackson <cjacks34@syr.edu>
2025-09-28 20:46:01 +0000

    added ode solving functions

--- functions.py
+++ functions.py
@@ -1,22 +1,109 @@
 import numpy as np
 
 
+###################### ODE METHODS ################################################################
+
+
+def state(t, I, dot_I, R, L, C):
+    out1 = dot_I
+    out2 = (-R / L) * dot_I + (-1 / (L * C)) * I
+
+    return np.array([out1, out2])
+
+
 def euler_explicit(
-    force, vel_init, m, dt
+    state, initial_I, initial_dot_I, start_time, end_time, N, R, L, C
 ):  # This function will take in some input ODE and evolve it according to the Euler Explicit rules
-    a = force / m
-    vel_final = vel_init + a * dt
 
-    return vel_final
+    delta_t = (end_time - start_time) / N
 
+    t = np.empty(N)
+    dot_I = np.empty(N)
+    I = np.empty(N)
+
+    t[0] = start_time
+    dot_I[0] = initial_dot_I
+    I[0] = initial_I
+
+    for i in range(0, N - 1):
+        dot_I[i + 1] = dot_I[i] + delta_t * state(t[i], I[i], dot_I[i], R, L, C)[1]
+        I[i + 1] = I[i] + delta_t * state(t[i], I[i], dot_I[i], R, L, C)[0]
+        t[i + 1] = t[i] + delta_t
+
+    return (I, dot_I, t)
+
+
+def runge_kutta4(state, initial_I, initial_dot_I, start_time, end_time, N, R, L, C):
+
+    delta_t = (end_time - start_time) / N
+
+    t = np.empty(N)
+    dot_I = np.empty(N)
+    I = np.empty(N)
+
+    t[0] = start_time
+    dot_I[0] = initial_dot_I
+    I[0] = initial_I
+
+    for i in range(0, N - 1):
+        k1f = delta_t * state(t[i], I[i], dot_I[i], R, L, C)[0]
+        k1g = delta_t * state(t[i], I[i], dot_I[i], R, L, C)[1]
+
+        k2f = (
+            delta_t
+            * state(t[i] + (delta_t / 2), I[i] + (k1f / 2), dot_I + (k1g / 2), R, L, C)[
+                0
+            ]
+        )
+        k2g = (
+            delta_t
+            * state(t[i] + (delta_t / 2), I[i] + (k1f / 2), dot_I + (k1g / 2), R, L, C)[
+                1
+            ]
+        )
+
+        k3f = (
+            delta_t
+            * state(t[i] + (delta_t / 2), I[i] + (k2f / 2), dot_I + (k2g / 2), R, L, C)[
+                0
+            ]
+        )
+        k3g = (
+            delta_t
+            * state(t[i] + (delta_t / 2), I[i] + (k2f / 2), dot_I + (k2g / 2), R, L, C)[
+                1
+            ]
+        )
+
+        k4f = delta_t * state(t[i] + delta_t, I[i] + k3f, dot_I + k3g, R, L, C)[0]
+        k4g = delta_t * state(t[i] + delta_t, I[i] + k3f, dot_I + k3g, R, L, C)[1]
+
+        I[i + 1] = I[i] + (1 / 6) * (k1f + 2 * k2f + 2 * k3f + k4f)
+        dot_I[i + 1] = dot_I[i] + (1 / 6) * (k1g + 2 * k2g + 2 * k3g + k4g)
+        t[i + 1] = t[i] + delta_t
+
+    return (I, dot_I, t)
 
 
 ######################## INTEGRATION METHODS ###################################################
 
-def riemann(func, lower: float, upper: float, N: float):
 
+def riemann(func, lower: float, upper: float, N: float):
+    """
+    Compute the definite integral of a function using the left hand Riemann sum.
+
+    Args:
+        func: function to integrate
+        lower: lower bound of integration
+        upper: upper bound of integration
+        N: number of steps for integration
+
+    Returns:
+        integral: final integrated value
+
+    """
     dx = (upper - lower) / N  # step size
-    
+
     integral = 0
 
     for k in range(1, N):
